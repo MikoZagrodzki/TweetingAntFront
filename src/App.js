5fb86af0ea2cc804204f14a2debc0fb0dbcdcfd8
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-02-03 13:00:18 +0000

    MVP fully working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [buttonClick, setButtonClick] = useState(false);
   const [responseArray, setResponseArray] = useState([]);
   const [numberSentences, setNumberSentences] = useState(10);
+  const [isResponseEmpty, setResponseEmpty] = useState(false);
 
   //to my byc wywolywane na chatGptResponsee✅
   //array✅
@@ -43,10 +44,15 @@ function App() {
       });
       if (!response.ok) {
         throw new Error("Please reload the app");
-      }
+      } 
       const chatResponse = await response.json();
       setChatGptResponse(chatResponse.data);
       console.log(chatResponse);
+
+      if (!chatResponse.data) {
+        setResponseEmpty(!isResponseEmpty)
+      }
+      
     } catch (error) {
       console.log(error.message);
     }
@@ -60,7 +66,7 @@ function App() {
       return;
     }
     getApiGpt();
-  }, [buttonClick, responseArray]);
+  }, [buttonClick, responseArray,isResponseEmpty]);
 
   const handleResponse = () => {
     if (responseArray.length === 0) {
@@ -99,7 +105,7 @@ function App() {
 
       <button onClick={() => setButtonClick(!buttonClick)}>test</button>
       <p>{chatGptResponse}</p>
-      <h1>{responseArray}</h1>
+      <p>{responseArray}</p>
     </div>
   );
 }
